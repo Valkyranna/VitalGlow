91756a63e82ced8624eef207375878719815a3e1
Valkyranna <103472677+Valkyranna@users.noreply.github.com>
2025-10-04 15:00:49 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'dist',
   trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
